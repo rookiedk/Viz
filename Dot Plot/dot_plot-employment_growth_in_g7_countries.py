f6cdcd3a4167c68736682dd295c0ab79758919f1
rookiedk <deepika.anandakrishnan@gmail.com>
2018-08-03 14:16:10 -0400

    Dot Plot for August 2018 SWD Challenge

---
+++ Dot Plot/dot_plot-employment_growth_in_g7_countries.py
@@ -0,0 +1,66 @@
+#Temp
+import matplotlib.ticker as plticker
+import pandas as pd
+import numpy as np
+import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style("white")
+plt.xkcd();
+#Load data
+data_source = 'C:\Users\danandak\Desktop\\temp\Employment Growth in G-7 Countries.xlsx'
+df = pd.read_excel(data_source)
+df['Employment Share'] = df['Employment Share'] * 100
+df['Net Employment Growth Share'] = df['Net Employment Growth Share'] * 100
+
+#Prepare Data
+y_labels = df['Country'].tolist()
+y_pos = np.arange(len(y_labels))
+x1 = df['Employment Share'].tolist()
+x2 = df['Net Employment Growth Share'].tolist()
+gs = gridspec.GridSpec(1, 1)
+gs.update(wspace=0.1, hspace=0.4)
+#Actual Plot
+ax = plt.subplot(gs[0, 0])
+for i in range(0, len(y_labels)):
+    ax.plot((x1[i]),(y_pos[i]), 'o', color='dodgerblue', zorder=5, label='Employment Share' if i == 0 else "")
+    ax.plot((x2[i]), (y_pos[i]), 'o', color='darkblue', zorder=5,  label='Net Employment Growth' if i == 0 else "")
+    if x1[i] > x2[i]:
+        color = 'green'
+    else:
+        color='red'
+    if 'United Kingdom' in y_labels[i]:
+        ax.text(x1[i]-6, y_pos[i], y_labels[i])
+    elif 'Japan' in y_labels[i]:
+        ax.text(x2[i] - 3, y_pos[i], y_labels[i])
+    elif 'Italy' in y_labels[i]:
+        ax.text(x2[i] - 3, y_pos[i], y_labels[i])
+    elif 'Germany' in y_labels[i]:
+        ax.text(x1[i] - 4, y_pos[i], y_labels[i])
+    elif 'France' in y_labels[i]:
+        ax.text(x2[i] - 3, y_pos[i], y_labels[i])
+    elif 'Canada' in y_labels[i]:
+        ax.text(x1[i] - 3, y_pos[i], y_labels[i])
+    else:
+        ax.text(x1[i] - 6, y_pos[i], y_labels[i])
+    if i==0 or i==2:
+        ax.plot((x1[i], x2[i]), (y_pos[i],  y_pos[i]), color=color, linewidth=1.5,
+            label='Employment is greater than Net Employment Growth' if 'green' in color else 'Employment is lesser than Net Employment Growth')
+    else:
+        ax.plot((x1[i], x2[i]), (y_pos[i], y_pos[i]), color=color, linewidth=1.5)
+ax.legend(loc='best')
+ax.annotate('Canada has the lowest difference with\nEmployment at 5.1% and Net Employment Growth at 5.2% Net', xy=(5.5, 1), xytext=(15,0.8),arrowprops=dict(arrowstyle="->",
+                            connectionstyle="angle3,angleA=0,angleB=-90"))
+ax.annotate('USA has the highest difference with\nEmployment at 42.0% and Net Employment Growth at 55.9% Net', xy=(40, 0.2), xytext=(40,1.4),arrowprops=dict(arrowstyle="->",
+                            connectionstyle="angle3,angleA=0,angleB=-90"))
+ax.set_title('Employment Growth in G-7 Countries\n(Source : Business Insider)')
+ax.text(50, 6.2, '')
+# Hide the right and top spines
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.spines['left'].set_visible(False)
+ax.tick_params(left=False)
+ax.tick_params(labelleft=False)
+ax.xaxis.set_major_formatter(plticker.FormatStrFormatter('%.0f%%'))
+
+plt.show()
